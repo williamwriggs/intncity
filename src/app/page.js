7563e592d4e288c5fc9e71c21379dc102d2e1d9d
pjol <peej@oleary.com>
2024-09-25 14:27:42 -0700

    Typography pt 5

--- src/app/page.js
+++ src/app/page.js
@@ -167,10 +167,12 @@ export default function StartTreePlantingRequest() {
             <Typography component="h1" variant="h4" color="primary" pb={4} align="center">
               Better Neighborhoods. Same Neighbors.
             </Typography>
-            <Typography component="h2" variant="h6" color="secondary" align="center">
-              {`We're trying to make planting trees more efficient through the
-              power of community—starting in the City of Oakland.`}
-            </Typography>
+            <div style={{width: "80%"}}>
+              <Typography component="h2" variant="h6" color="secondary" align="center">
+                {`We're trying to make planting trees more efficient through the
+                power of community—starting in the City of Oakland.`}
+              </Typography>
+            </div>
             <Box
               component="form"
               onSubmit={handleSubmit}
